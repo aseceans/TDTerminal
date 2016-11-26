a189ad73c94bd47a550b4aa0a0e093b539cfc8a8
ryanmunro3 <ryan.munro3@gmail.com>
2016-11-26 01:29:50 -0500

    first commit

---
+++ TDTerminal/app/src/main/java/com/rrm/tdterminal/TerminalActivity.java
@@ -0,0 +1,118 @@
+package com.rrm.tdterminal;
+
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Parcelable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class TerminalActivity extends AppCompatActivity {
+    String VendorName = "Fanshawe College";
+    NfcAdapter nfc;
+    EditText myET;
+    TextView myTV;
+    TextView myOutput;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        pubnubTerminalService.currentActivity = this;
+        setContentView(R.layout.activity_terminal);
+        myET = (EditText)findViewById(R.id.priceEdit);
+        myTV = (TextView)findViewById(R.id.connectTV);
+        myOutput = (TextView)findViewById(R.id.textTagContent);
+        pubnubTerminalService.PubnubConnect();
+        nfc = NfcAdapter.getDefaultAdapter(this);
+        if(nfc != null && nfc.isEnabled()){
+            Toast.makeText(this, "NFC available!", Toast.LENGTH_SHORT).show();
+        }
+        else
+            finish();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent)
+    {
+        Toast.makeText(this, "NFC intent received!!", Toast.LENGTH_SHORT).show();
+
+        Parcelable[] parcelables = intent.getParcelableArrayExtra(nfc.EXTRA_NDEF_MESSAGES);
+        if(parcelables != null && parcelables.length > 0)
+        {
+            readTextFromMessage((NdefMessage) parcelables[0]);
+        }else
+        {
+            Toast.makeText(this, "No Message!!", Toast.LENGTH_SHORT).show();
+        }
+
+        super.onNewIntent(intent);
+    }
+
+    private void readTextFromMessage(NdefMessage ndef) {
+        NdefRecord[] ndefRecords = ndef.getRecords();
+        if(ndefRecords != null && ndefRecords.length > 0)
+        {
+            NdefRecord ndefrecord = ndefRecords[0];
+            byte[] payload = ndefrecord.getPayload();
+            String payloadString = new String(payload);
+            payloadString = payloadString.substring(3);
+            myOutput.setText(payloadString);
+            double myD = Double.parseDouble(myET.getText().toString());
+            ARMessageRequest myARMR = new ARMessageRequest(VendorName, payloadString, myD);
+            pubnubTerminalService.SendMessage(pubnubTerminalService.REQUEST, myARMR);
+        }else {
+            Toast.makeText(this, "No Records!!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onPause()
+    {
+        nfc.disableForegroundDispatch(this);
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        Intent intent = new Intent(this, TerminalActivity.class);
+        intent.addFlags(Intent.FLAG_RECEIVER_REPLACE_PENDING);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        IntentFilter[] intentFilter = new IntentFilter[]{};
+        nfc.enableForegroundDispatch(this, pendingIntent, intentFilter, null);
+        super.onResume();
+    }
+
+    // Define a handle to access the context on the main thread. This Handler will handle room created messages.
+    private Handler roomCreatedHandle = new Handler()
+    {
+        @Override
+        public void handleMessage(Message msg)
+        {
+            String connectString = "Connected to: " + pubnubTerminalService.ROOM_NAME;
+            myTV.setText(connectString);
+        }
+    };
+
+    private Handler messageHandle = new Handler()
+    {
+        @Override
+        public void handleMessage(Message msg)
+        {
+            Bundle data = msg.getData();
+            String mydata = data.getString(pubnubTerminalService.RESPONSE);
+            Toast toast = Toast.makeText(pubnubTerminalService.currentActivity, mydata, Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    };
+
+    public Handler getRoomCreatedHandle() {return roomCreatedHandle;}
+    public Handler getMessageHandle() {return messageHandle;}
+}
---
+++ TDTerminal/app/src/main/java/com/rrm/tdterminal/pubnubTerminalService.java
@@ -0,0 +1,104 @@
+package com.rrm.tdterminal;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Message;
+
+import com.google.gson.Gson;
+import com.pubnub.api.Callback;
+import com.pubnub.api.Pubnub;
+import com.pubnub.api.PubnubError;
+import com.pubnub.api.PubnubException;
+
+/**
+ * Created by Ryan on 11/24/2016.
+ */
+
+public class pubnubTerminalService {
+    public static Gson gson = new Gson();
+    public static final String ROOM_NAME = "arDemo";
+    public static Context context; // don't touch this
+    public static Context currentActivity;
+    public static boolean terminal = true;
+    public static final String RESPONSE = "RESPONSE";
+    public static final String REQUEST = "REQUEST";
+
+    // Pubnub Keys
+    private static String publishKey = "pub-c-3fde3c33-2104-4a30-be52-710969bfb4b3";
+    private static String subscribeKey = "sub-c-45d16f72-b136-11e6-a7bb-0619f8945a4f";
+
+    private static Pubnub pubnub = new Pubnub(publishKey, subscribeKey, true);
+
+    public static Pubnub getPubnub()
+    {
+        return pubnub;
+    }
+
+    public static void SendMessage(String message_type, Object message_data)
+    {
+        //create the message object
+        ARMessage message = new ARMessage(message_type, message_data);
+
+        // serialize the message object
+        String messageJson = gson.toJson(message);
+
+        // send the message
+        getPubnub().publish(ROOM_NAME, messageJson, new Callback() { });
+    }
+
+    public static void ReceivedMessage(Object message)
+    {
+        ARMessageResponse response = new ARMessageResponse();
+        ARMessage arMessage = gson.fromJson(message.toString(), ARMessage.class);
+
+        switch(arMessage.Type)
+        {
+            case(RESPONSE): {
+                response = gson.fromJson(arMessage.Data, ARMessageResponse.class);
+                Message m = ((TerminalActivity) currentActivity).getMessageHandle().obtainMessage();
+                Bundle data = new Bundle();
+                data.putString(RESPONSE, response.Message);
+                m.setData(data);
+                m.sendToTarget();
+                break;
+            }
+            case(REQUEST):
+                break;
+        }
+    }
+
+    public static void PubnubConnect()
+    {
+        try
+        {
+            pubnub.subscribe(ROOM_NAME, new Callback()
+            {
+                @Override
+                public void connectCallback(String channel_name, Object message)
+                {
+                    {
+                        // Notify the presenter that the room was created
+                        Message m = ((TerminalActivity)currentActivity).getRoomCreatedHandle().obtainMessage();
+                        m.sendToTarget();
+                    }
+                }
+
+                @Override
+                public void successCallback(String channel_name, Object message)
+                {
+                    ReceivedMessage(message);
+                }
+
+                @Override
+                public void errorCallback(String channel_name, PubnubError error)
+                {
+
+                }
+            });
+        }
+        catch (PubnubException e)
+        {
+            System.out.println(e.toString());
+        }
+    }
+}
